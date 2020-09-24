8754692e265abaac426678dd93df062c2546d50a
root6439 <nicolas.soaresh.98@gmail.com>
2020-09-24 17:06:05 -0300

    crição dos componentes topo e painel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { TopoComponent } from './topo/topo.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
