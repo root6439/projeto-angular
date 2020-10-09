f0d191b8df85a1267ed063fa3dcf58fb2829782e
Nicolas Soares Henrique <henriquens@saude.sc.gov.br>
2020-10-09 12:33:25 -0300

    Mais alterações

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,5 +1,6 @@
 import { FRASES } from './frases.mock';
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frase.model';
 
 @Component({
   selector: 'app-painel',
@@ -9,16 +10,35 @@ import { Component, OnInit } from '@angular/core';
 export class PainelComponent implements OnInit {
 
   public frases = FRASES;
-  public resposta
+  public resposta: string;
 
-  constructor() { }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  public progresso: number = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+  }
 
   ngOnInit(): void {
   }
 
   public atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
-    console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('Acertou');
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+      this.progresso += (100 / this.frases.length);
+    } else {
+      alert('Errou');
+    }
+
   }
 
 }
